4b22bc08bf2fba247089e056920b7b0115811ea3
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-07 14:04:13 -0300

    Initialize Map Detail

--- src/init/init.c
+++ src/init/init.c
@@ -6,11 +6,14 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 15:21:54 by rlins             #+#    #+#             */
-/*   Updated: 2023/03/07 11:49:09 by rlins            ###   ########.fr       */
+/*   Updated: 2023/03/07 12:07:06 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+
+static void init_map_det(t_map_det *map_det);
+
 // TODO: Remover?
 int	start(int argc, char **argv)
 {
@@ -23,4 +26,20 @@ void init_data(t_data *data)
 	data->mlx = NULL;
 	data->win = NULL;
 	data->map = NULL;
+	init_map_det(&data->map_det);
+	// TODO: Provavelmente haverá um initialize person too
+}
+
+/**
+ * @brief Initialize the map detail structure.
+ * @param map_det Map Detail Structure by ref
+ */
+static void init_map_det(t_map_det *map_det)
+{
+	map_det->fd = 0;
+	map_det->lines_count = 0;
+	map_det->path = NULL;
+	map_det->file = NULL;
+	map_det->width = 0;
+	map_det->height = 0;
 }
