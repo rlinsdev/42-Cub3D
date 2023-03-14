9d59036af54cac5e5dbb446ae2157513634e56ce
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-14 16:47:30 -0300

    Add invalid maps

--- include/define.h
+++ include/define.h
@@ -27,7 +27,7 @@
 # define ERR_TEXT_MAP "Texture not found in file."
 # define ERR_TEXT_COL "Color not found in file."
 # define ERR_TEXT_PATH "Error in Texture. Invalid path."
-# define ERR_RGB_VAL "Invalid RGB Value. 0 to 255."
+# define ERR_RGB_VAL "Invalid RGB Value. Check the file passed by param."
 
 # define ERR_MAP_CHAR "Character invalid in map."
 # define ERR_SING_PLAYER "Map error. Just one player allowed"
