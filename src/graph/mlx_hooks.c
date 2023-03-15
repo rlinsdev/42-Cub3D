d12f9732de9325d7cd02ba4184ec272f4f583068
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-15 09:49:42 -0300

    Fixing compilation bug

--- src/graph/mlx_hooks.c
+++ src/graph/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 12:24:14 by lucas             #+#    #+#             */
-/*   Updated: 2023/03/11 13:19:44 by rlins            ###   ########.fr       */
+/*   Updated: 2023/03/15 09:48:21 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	handle_hook_close(t_data *data)
 	mlx_destroy_image(data->view.mlx, data->view.screen.img);
 	mlx_destroy_image(data->view.mlx, data->view.minimap.img);
 	mlx_destroy_window(data->view.mlx, data->view.win);
-	// mlx_destroy_display(data->view.mlx); // Todo: Comentado p/ compilar
+	mlx_destroy_display(data->view.mlx);
 
 	free_data(data);
 	free(data->view.mlx);
