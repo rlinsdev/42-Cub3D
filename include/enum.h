de4187ff111f29ba88a971042df7580f387e8564
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-30 11:28:53 -0300

    add enum texture

--- include/enum.h
+++ include/enum.h
@@ -5,9 +5,16 @@ enum	e_output
 {
 	SUCCESS = 0,
 	FAILURE = 1,
-	// ERROR = 2,
 	BREAK = 3,
 	CONTINUE = 4
+};
+
+enum e_texture_index
+{
+	NORTH = 0,
+	SOUTH = 1,
+	EAST = 2,
+	WEST = 3
 };
 
 # endif
