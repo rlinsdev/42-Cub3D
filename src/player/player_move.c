64cc0fcd1145b147d0be1292b3874d8f5bec8960
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-30 11:44:59 -0300

    Player Move

---
+++ src/player/player_move.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_move.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 08:37:54 by rlins             #+#    #+#             */
+/*   Updated: 2023/03/30 11:44:50 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	validate_move(t_data *data, double new_x, double new_y);
+//TODO:L
+static int	move_player_forward(t_data *data)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = data->player.pos_x + data->player.dir_x * MOVESPEED;
+	new_y = data->player.pos_y + data->player.dir_y * MOVESPEED;
+	return (validate_move(data, new_x, new_y));
+}
+//TODO:L
+static int	move_player_backward(t_data *data)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = data->player.pos_x - data->player.dir_x * MOVESPEED;
+	new_y = data->player.pos_y - data->player.dir_y * MOVESPEED;
+	return (validate_move(data, new_x, new_y));
+}
+//TODO:L
+static int	move_player_left(t_data *data)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = data->player.pos_x + data->player.dir_y * MOVESPEED;
+	new_y = data->player.pos_y - data->player.dir_x * MOVESPEED;
+	return (validate_move(data, new_x, new_y));
+}
+//TODO:L
+static int	move_player_right(t_data *data)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = data->player.pos_x - data->player.dir_y * MOVESPEED;
+	new_y = data->player.pos_y + data->player.dir_x * MOVESPEED;
+	return (validate_move(data, new_x, new_y));
+}
+
+int	move_player(t_data *data)
+{
+	int	moved;
+
+	moved = 0;
+	if (data->player.move_y == 1)
+		moved += move_player_forward(data);
+	if (data->player.move_y == -1)
+		moved += move_player_backward(data);
+	if (data->player.move_x == -1)
+		moved += move_player_left(data);
+	if (data->player.move_x == 1)
+		moved += move_player_right(data);
+	if (data->player.rotate != 0)
+		moved += rotate_player(data, data->player.rotate);
+	return (moved);
+}
+
+//TODO:L Entender porque muro externo da certo, mas muro interno não da
+static bool	is_valid_pos_wall_collision(t_data *data, double x, double y)
+{
+	if (data->map[(int)y][(int)x] == '0')
+		return (true);
+	return (false);
+}
+//TODO:L
+static bool	is_valid_pos_in_map(t_data *data, double x, double y)
+{
+	if (x < 0.25 || x >= data->map_det.width - 1.25)
+		return (false);
+	if (y < 0.25 || y >= data->map_det.height -0.25)
+		return (false);
+	return (true);
+}
+//TODO:L
+static bool	is_valid_pos(t_data *data, double x, double y)
+{
+	if (is_valid_pos_in_map(data, x, y))
+		return (true);
+	return (false);
+}
+//TODO:L Classe de validação né....
+int	validate_move(t_data *data, double new_x, double new_y)
+{
+	int	moved;
+
+	moved = 0;
+	if (is_valid_pos(data, new_x, data->player.pos_y))
+	{
+		data->player.pos_x = new_x;
+		moved = 1;
+	}
+	if (is_valid_pos(data, data->player.pos_x, new_y))
+	{
+		data->player.pos_y = new_y;
+		moved = 1;
+	}
+	return (moved);
+}
+
