3d3603e6c24b14c217a803b6baa880569b11bd18
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-30 11:42:13 -0300

    Initialize Data

--- src/init/init_data.c
+++ src/init/init_data.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 15:21:54 by rlins             #+#    #+#             */
-/*   Updated: 2023/03/28 16:23:24 by rlins            ###   ########.fr       */
+/*   Updated: 2023/03/30 11:41:59 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,67 @@ static void init_ray(t_ray *r, t_data *data);
  * @brief Initialize Ray values
  * @param r
  */
+
+//TODO:L
 static void init_ray(t_ray *r, t_data *data)
 {
-	r->dir[0] = 0;
-	r->dir[1] = 0;
-	r->plane[0] = 0;
-	r->plane[1] = 0;
-	r->speed = 0.1;
+	// r->dir[0] = 0;
+	// r->dir[1] = 0;
+	// r->plane[0] = 0;
+	// r->plane[1] = 0;
+	// r->speed = 0.1;
 }
 
-void	set_player_direction(t_ray *r)
+//TODO:L
+void	set_player_direction(t_player *p)
 {
-	if (r->dir_char == 'N')
+	// if (r->dir_char == 'N')
+	// {
+	// 	r->dir[0] = -1;
+	// 	r->plane[1] = -0.66;
+	// }
+	// else if (r->dir_char == 'S')
+	// {
+	// 	r->dir[0] = 1;
+	// 	r->plane[1] = 0.66;
+	// }
+	// else if (r->dir_char == 'E')
+	// {
+	// 	r->dir[1] = 1;
+	// 	r->plane[0] = -0.66;
+	// }
+	// else if (r->dir_char == 'W')
+	// {
+	// 	r->dir[1] = -1;
+	// 	r->plane[0] = 0.66;
+	// }
+	if (p->dir == 'S')
 	{
-		r->dir[0] = -1;
-		r->plane[1] = -0.66;
+		p->dir_x = 0;
+		p->dir_y = 1;
+		p->plane_x = -0.66;
+		p->plane_y = 0;
 	}
-	else if (r->dir_char == 'S')
+	else if (p->dir == 'N')
 	{
-		r->dir[0] = 1;
-		r->plane[1] = 0.66;
+		p->dir_x = 0;
+		p->dir_y = -1;
+		p->plane_x = 0.66;
+		p->plane_y = 0;
 	}
-	else if (r->dir_char == 'E')
+	if (p->dir == 'W')
 	{
-		r->dir[1] = 1;
-		r->plane[0] = -0.66;
+		p->dir_x = -1;
+		p->dir_y = 0;
+		p->plane_x = 0;
+		p->plane_y = -0.66;
 	}
-	else if (r->dir_char == 'W')
+	else if (p->dir == 'E')
 	{
-		r->dir[1] = -1;
-		r->plane[0] = 0.66;
+		p->dir_x = 1;
+		p->dir_y = 0;
+		p->plane_x = 0;
+		p->plane_y = 0.66;
 	}
 }
 
@@ -55,4 +86,7 @@ void	init_data(t_data *data)
 {
 	ft_bzero(data, sizeof(t_data));
 	init_ray(&data->ray, data);
+	data->win_width = WIDTH;
+	data->win_height = HEIGHT; // TODO: Ver se precisa destsa 2 variaveis mesmo
+	//TODO:L
 }
