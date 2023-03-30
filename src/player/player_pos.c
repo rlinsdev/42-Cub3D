a2290e96951b7af46587d2526256ac2307422a29
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-30 11:45:50 -0300

    Player Position

---
+++ src/player/player_pos.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_pos.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 08:47:01 by rlins             #+#    #+#             */
+/*   Updated: 2023/03/30 11:45:37 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "cub3d.h"
+//TODO:L melhorar o nome rot speed 
+static int	rotate_left_right(t_data *data, double rotspeed)
+{
+	t_player	*p;
+	double		tmp_x;
+
+	p = &data->player;
+	tmp_x = p->dir_x;
+	p->dir_x = p->dir_x * cos(rotspeed) - p->dir_y * sin(rotspeed);
+	p->dir_y = tmp_x * sin(rotspeed) + p->dir_y * cos(rotspeed);
+	tmp_x = p->plane_x;
+	p->plane_x = p->plane_x * cos(rotspeed) - p->plane_y * sin(rotspeed);
+	p->plane_y = tmp_x * sin(rotspeed) + p->plane_y * cos(rotspeed);
+	return (1);
+}
+//TODO:L Melhorar o nome 'rotdir
+int	rotate_player(t_data *data, double rotdir)
+{
+	int		moved;
+	double	rotspeed;
+
+	moved = 0;
+	rotspeed = ROTSPEED * rotdir;
+	moved += rotate_left_right(data, rotspeed);
+	return (moved);
+}
