62c0c4ba89cae6c438a3261748cc1f7a8461491c
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-30 22:07:49 -0300

    Debug

--- src/util/debug.c
+++ src/util/debug.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 10:20:05 by rlins             #+#    #+#             */
-/*   Updated: 2023/03/30 16:05:54 by rlins            ###   ########.fr       */
+/*   Updated: 2023/03/30 22:01:04 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,44 @@
 static void	debug_player(t_player *p);
 static void	debug_ray(t_ray *r);
 
-
-//TODO:L Implementar
+/**
+ * @brief Show the values of variables in player structure
+ * @param p Player struucture
+ */
 static void	debug_player(t_player *p)
 {
 	printf("\n****Player****\n");
+	printf("dir: %d\n", p->dir);
 	printf("pos_x: %f\n", p->pos_x);
+	printf("pos_y: %f\n", p->pos_y);
+	printf("dir_x: %f\n", p->dir_x);
+	printf("dir_y: %f\n", p->dir_y);
+	printf("plane_x: %f\n", p->plane_x);
+	printf("plane_y: %f\n", p->plane_y);
+	printf("move_x: %d\n", p->move_x);
+	printf("move_y: %d\n", p->move_y);
+	printf("has_moved: %d\n", p->has_moved);
+	printf("rotate: %d\n", p->rotate);
 }
 
 /**
- * @brief
- *
- * @param r
+ * @brief Show the values of variables in ray structure
+ * @param r Ray Structure
  */
 static void	debug_ray(t_ray *r)
 {
 	printf("\n****Ray****\n");
+
+	printf("camera: %f\n", r->camera_x);
+	printf("dir_x: %f\n", r->dir_x);
+	printf("dir_y: %f\n", r->dir_y);
+
+	printf("map_x: %d\n", r->map_x);
+	printf("map_y: %d\n", r->map_y);
+
+	printf("step_x: %d\n", r->step_x);
+	printf("step_y: %d\n", r->step_y);
+
 	printf("HitSide: %s\n", r->hit_side ? "true" : "false");
 }
 
